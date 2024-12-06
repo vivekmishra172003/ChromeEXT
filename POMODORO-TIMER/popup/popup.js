f805d21deff27a5e81ab9237c4062a2916ee6316
vivekmishra172003 <vivekmishra172003@gmail.com>
2024-12-06 10:32:32 +0530

    setup is done and  some basic buttoon and add button is done

---
+++ POMODORO-TIMER/popup/popup.js
@@ -0,0 +1,23 @@
+const tasks =[]
+
+const addTaskBtn = document.getElementById('add-task-btn');
+addTaskBtn.addEventListener("click",()=>addTask());
+
+function addTask(){
+    const taskRow = document.createElement('div');
+    const text = document.createElement('input');
+    text.type = 'text';
+    text.placeholder = 'Enter Task';
+    text.addEventListener("change",)
+
+    const deleteBtn = document.createElement('input');
+    deleteBtn.type = "button";
+    deleteBtn.value = "X";
+
+    taskRow.appendChild(text);
+    taskRow.appendChild(deleteBtn); 
+
+    const taskContainer = document.getElementById('task-container');
+    taskContainer.appendChild(taskRow);
+
+}
