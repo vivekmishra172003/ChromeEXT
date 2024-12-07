b3b06f1b13a65f4f16b414d97011ca81212b3942
vivekmishra172003 <vivekmishra172003@gmail.com>
2024-12-07 08:14:31 +0530

    add task,render task,delete tas{some portion is remaining}k

--- POMODORO-TIMER/popup/popup.js
+++ POMODORO-TIMER/popup/popup.js
@@ -3,21 +3,38 @@ const tasks =[]
 const addTaskBtn = document.getElementById('add-task-btn');
 addTaskBtn.addEventListener("click",()=>addTask());
 
-function addTask(){
+function renderTasks(taskNum){
     const taskRow = document.createElement('div');
     const text = document.createElement('input');
     text.type = 'text';
     text.placeholder = 'Enter Task';
-    text.addEventListener("change",)
+    text.addEventListener("change",()=>{
+        tasks[taskNum]=text.value;
+        console.log(tasks);
+    })
 
+    
     const deleteBtn = document.createElement('input');
     deleteBtn.type = "button";
     deleteBtn.value = "X";
+    deleteBtn.addEventListener("click",()=>{
 
+    })
     taskRow.appendChild(text);
     taskRow.appendChild(deleteBtn); 
 
     const taskContainer = document.getElementById('task-container');
     taskContainer.appendChild(taskRow);
-
 }
+
+function addTask(){
+    const taskNum = tasks.length;
+    tasks.push("")
+    renderTasks(taskNum);
+}
+
+function deleteTask(taskNum){
+    tasks.splice(taskNum,1);
+}
+
+// 
